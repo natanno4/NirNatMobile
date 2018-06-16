5c54aaa06ed7ba30bb66affc61f828ae69efca01
natanno4 <natanno4@gmail.com>
2018-06-16 13:06:46 +0300

    onCreate

--- app/src/main/java/com/imageservice/natan/imageservicemobile/ImageServiceService.java
+++ app/src/main/java/com/imageservice/natan/imageservicemobile/ImageServiceService.java
@@ -10,12 +10,16 @@ import android.graphics.Bitmap;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
+import android.os.Environment;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.widget.Toast;
 
 import java.io.ByteArrayOutputStream;
+import java.io.File;
 import java.io.OutputStream;
+import java.net.InetAddress;
 import java.net.Socket;
 
 public class ImageServiceService extends Service {
@@ -29,8 +33,16 @@ public class ImageServiceService extends Service {
         return null;
     }
 
-    public int onStartCommand(Intent intent, int flag, int startId) {
+    @Override
+    public void onCreate() {
+        super.onCreate();
         Toast.makeText(this, "service is starting...", Toast.LENGTH_SHORT);
+        try {
+            InetAddress serverAddr = InetAddress.getByName("10.0.0.2");
+            this.socket = new Socket(serverAddr, 8600);
+        } catch (Exception e) {
+            Log.e("TCP", "c:Error", e);
+        }
         final IntentFilter theFilter = new IntentFilter();
         theFilter.addAction("android.new.wifi.supplicant.CONNECTION_Change");
         theFilter.addAction("android.net.wifi.STATE_CHANGE");
@@ -49,12 +61,6 @@ public class ImageServiceService extends Service {
             }
         };
         this.registerReceiver(this.wifiReceiver, theFilter);
-        return START_STICKY;
-    }
-
-    @Override
-    public void onCreate() {
-        super.onCreate();
 
     }
 
@@ -63,7 +69,15 @@ public class ImageServiceService extends Service {
     }
 
     public void startTransfer() {
+        
+    }
 
+    public File[] getAllCameraPhotos() {
+        File dcim = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+        if(dcim == null) {
+            return null;
+        }
+        return dcim.listFiles();
     }
 
     public byte[] getBytesFromBitmap(Bitmap bitmap) {
