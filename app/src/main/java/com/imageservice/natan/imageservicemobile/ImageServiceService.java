42c532d6dcdd8e2a46b8d0494126e7514638d04b
natanno4 <natanno4@gmail.com>
2018-06-19 14:39:47 +0300

    add ImageServerTransfer

--- app/src/main/java/com/imageservice/natan/imageservicemobile/ImageServiceService.java
+++ app/src/main/java/com/imageservice/natan/imageservicemobile/ImageServiceService.java
@@ -7,25 +7,27 @@ import android.content.Context;
 import android.content.Intent;
 import android.content.IntentFilter;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
 import android.net.wifi.WifiManager;
 import android.os.Environment;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
+import android.support.v4.app.NotificationManagerCompat;
 import android.util.Log;
 import android.widget.Toast;
-
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.FileInputStream;
 import java.io.OutputStream;
 import java.net.InetAddress;
 import java.net.Socket;
 
 public class ImageServiceService extends Service {
     private BroadcastReceiver wifiReceiver;
-    private Socket socket;
-    private OutputStream outputStream;
+    private int count = 0;
 
     @Nullable
     @Override
@@ -37,12 +39,6 @@ public class ImageServiceService extends Service {
     public void onCreate() {
         super.onCreate();
         Toast.makeText(this, "service is starting...", Toast.LENGTH_SHORT);
-        try {
-            InetAddress serverAddr = InetAddress.getByName("10.0.0.2");
-            this.socket = new Socket(serverAddr, 8600);
-        } catch (Exception e) {
-            Log.e("TCP", "c:Error", e);
-        }
         final IntentFilter theFilter = new IntentFilter();
         theFilter.addAction("android.new.wifi.supplicant.CONNECTION_Change");
         theFilter.addAction("android.net.wifi.STATE_CHANGE");
@@ -51,9 +47,9 @@ public class ImageServiceService extends Service {
             public void onReceive(Context context, Intent intent) {
                 WifiManager wifiManager = (WifiManager) context.getSystemService(context.WIFI_SERVICE);
                 NetworkInfo networkInfo = intent.getParcelableExtra(WifiManager.EXTRA_NETWORK_INFO);
-                if(networkInfo != null) {
-                    if(networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
-                        if(networkInfo.getState() == NetworkInfo.State.CONNECTED) {
+                if (networkInfo != null) {
+                    if (networkInfo.getType() == ConnectivityManager.TYPE_WIFI) {
+                        if (networkInfo.getState() == NetworkInfo.State.CONNECTED) {
                             startTransfer();
                         }
                     }
@@ -61,7 +57,6 @@ public class ImageServiceService extends Service {
             }
         };
         this.registerReceiver(this.wifiReceiver, theFilter);
-
     }
 
     public void onDestroy() {
@@ -69,7 +64,62 @@ public class ImageServiceService extends Service {
     }
 
     public void startTransfer() {
-        
+        final File[] pics = getAllCameraPhotos();
+        if(pics == null) {
+            return;
+        }
+        final int length = pics.length;
+        if(pics != null) {
+            final NotificationManagerCompat notificationManager = NotificationManagerCompat.from(this);
+            final int notify_id = 1;
+            final NotificationCompat.Builder builder = new NotificationCompat.Builder(this, "default");
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    builder.setContentTitle("Picture Transfer")
+                            .setContentText("Transfer in progress")
+                            .setPriority(NotificationCompat.PRIORITY_LOW);
+                    for (; count < length; ) {
+                        if (count == length / 2) {
+                            builder.setContentText("Half way through");
+                            notificationManager.notify(notify_id, builder.build());
+                        }
+                        builder.setProgress(length, count, false);
+                        notificationManager.notify(notify_id, builder.build());
+                    }
+                    // At the End
+                    builder.setContentText("Download complete")
+                            .setProgress(0, 0, false);
+                    notificationManager.notify(notify_id, builder.build());
+                }
+            }).start();
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    for (File pic : pics) {
+                        count++;
+                        try {
+                            InetAddress serverAddr = InetAddress.getByName("10.0.0.2");
+                            Socket socket = new Socket(serverAddr, 8600);
+                            try {
+                                OutputStream outputStream = socket.getOutputStream();
+                                FileInputStream fis = new FileInputStream(pic);
+                                Bitmap bm = BitmapFactory.decodeStream(fis);
+                                byte[] imgbyte = getBytesFromBitmap(bm);
+                                outputStream.write(imgbyte);
+                                outputStream.flush();
+                            } catch (Exception e) {
+                                Log.e("TCP", "S: Error", e);
+                            } finally {
+                                socket.close();
+                            }
+                        } catch (Exception e) {
+                            Log.e("TCP", "C: Error", e);
+                        }
+                    }
+                }
+            }).start();
+        }
     }
 
     public File[] getAllCameraPhotos() {
@@ -85,10 +135,4 @@ public class ImageServiceService extends Service {
         bitmap.compress(Bitmap.CompressFormat.PNG, 70, stream);
         return stream.toByteArray();
     }
-
-
-
-
-
-
 }
